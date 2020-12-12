930a5d4a30b64e614dc84bbae25619dfa99b2dc1
TCris-ols <teresa.cristols@gmail.com>
2020-12-12 20:20:07 -0300

    DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -62,7 +62,8 @@ public class MainViewController implements Initializable {
 		mainVBox.getChildren().addAll(newVBox.getChildren());
 		}
 		catch (IOException e) {
-			Alerts.showAlert("IO Exception","Error loading view", e.getMessage(),AlertType.ERROR);
+			//Alerts.showAlert("IO Exception","Error loading view", e.getMessage(),AlertType.ERROR);
+			e.printStackTrace();
 		}
 	}
 
